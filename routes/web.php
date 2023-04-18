d3938bfbb72920e5737fb436e662d831f0ae1aa1
MariaJesus18 <mariadejesusmdrs@gmail.com>
2023-04-18 12:07:59 -0300

    correcao de conflitos

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome');
 Route::view('/dashboard', 'dashboard');
 
-
 require __DIR__ . '/auth.php';
 require __DIR__ . '/user.php';
 require __DIR__ . '/student.php';
