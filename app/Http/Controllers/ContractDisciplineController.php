b9091713d8b6f8977e21585221b61841a6dd3911
ggabriel360p <109006007+gabriel360p@users.noreply.github.com>
2023-06-05 08:35:16 -0300

    Add files via upload

--- app/Http/Controllers/ContractDisciplineController.php
+++ app/Http/Controllers/ContractDisciplineController.php
@@ -35,7 +35,12 @@ class ContractDisciplineController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        ContractDiscipline::create([
+            'contract_id'=>$request->contract_id,
+            'subject_id'=>$request->subject_id,
+        ]); 
+
+        return redirect(route('/'));
     }
 
     /**
