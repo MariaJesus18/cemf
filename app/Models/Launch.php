855bd4a02ab2e1e9d6d04c5fbfe411619a728461
MARIA DE JESUS DOS SANTOS MEDEIROS <jesus.maria@academico.ifrn.edu.br>
2023-06-16 21:34:24 -0300

    crud launchs

--- app/Models/Launch.php
+++ app/Models/Launch.php
@@ -4,10 +4,46 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Launch extends Model
 {
     use HasFactory;
+    protected $table = 'launchs';
+
+    public function contract()
+    {
+        return $this->belongsTo(Contract::class);
+    }
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+    public function suppliers(): BelongsTo
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+    public function cashAccount(): BelongsTo
+    {
+        return $this->belongsTo(cashAccount::class);
+    }
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(TypeRelease::class);
+    }
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(LaunchCategory::class);
+    }
+    public function paymentMethod(): BelongsTo
+    {
+        return $this->belongsTo(paymentMethod::class);
+    }
+    public function unit(): BelongsTo
+    {
+        return $this->belongsTo(Unit::class);
+    }
 
     protected $fillable = [
         'contract', 
@@ -25,12 +61,13 @@ class Launch extends Model
         'discount',
         'postingAmount',
         'barcode',
-        'histórico',
+        'historic',
         'installment',
         'status',
-        'id_creatorUser',
-        'id_userChange',
+        'creatoruser_id',
+        'editoruser_id',
         'dateCreated',
         'changeDate',
+        'unit',
     ];
 }
