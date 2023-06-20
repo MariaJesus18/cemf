4914ff66c07f2a763edb5acf13ee62a097870a0c
Gabriel Costa <gabrielmark210@gmail.com>
2023-06-20 15:03:00 -0300

    verificar emails

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'welcome');
-Route::view('/dashboard', 'dashboard');
+Route::view('/dashboard', 'dashboard')->middleware('emailVerify');
 
 require __DIR__ . '/auth.php';
 require __DIR__ . '/user.php';
